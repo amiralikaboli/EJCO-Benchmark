4353d767c58276f2a431d912dae732b3cf1bfcfb
amascolo <12738821+amascolo@users.noreply.github.com>
2024-11-22 16:40:25 +0000

    Fixed script after path changes

--- 1_85x/__main__.py
+++ 1_85x/__main__.py
@@ -10,21 +10,17 @@ import re
 import statistics
 from typing import Final
 
+from helpers.benchmarks.shared import join_frames
 from helpers.constants import SECS_TO_MS
-from helpers.free_join import (
-    Record,
-    RecordMean,
-    SpecsGJ,
-    categorise,
-    mapper,
-    mean_ms,
-    to_frame,
-)
-from helpers.plot import merge
+from helpers.free_join.job import Record, SPECS_FIELDS, SpecsGJ
+from helpers.free_join.shared import RecordMean, categorise, mapper, mean_ms, to_frame
 
 FILE_DIR: Final[str] = os.path.dirname(os.path.realpath(__file__))
 
 RE_QUERY: Final[re.Pattern] = re.compile(r"^(\d+)([a-z])$")
+
+BASELINE_COL: Final[str] = "free-join (on server)"
+OURS_COL: Final[str] = "wcoj (on server)"
 
 
 def to_ms(record: RecordMean) -> RecordMean:
@@ -41,13 +37,16 @@ if __name__ == "__main__":
         data = json.load(f)
 
     records = [Record.model_validate_json(json.dumps(item)) for item in data["gj"]]
-    [baseline] = categorise(records, SpecsGJ)
+    [baseline] = categorise(SPECS_FIELDS)(records, SpecsGJ)
     baseline_df = to_frame(mapper(mean_ms)(baseline))
 
     ours = (RecordMean.model_validate_json(json.dumps(item)) for item in data["ours"])
     ours_df = to_frame(sorted(map(to_ms, ours), key=split_query))
 
-    df = merge(baseline=baseline_df, ours=ours_df)
+    df = join_frames([BASELINE_COL, OURS_COL], [baseline_df, ours_df])
+    df["Performance Improvement"] = (df[BASELINE_COL] / df[OURS_COL]).round(2)
+    df.sort_values(by="Performance Improvement", ascending=False, inplace=True)
+
     geometric_mean = round(statistics.geometric_mean(df["Performance Improvement"]), 2)
     assert geometric_mean == 1.85
 
